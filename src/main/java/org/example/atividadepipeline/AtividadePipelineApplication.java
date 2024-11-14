48934c63bc62d052e40dff5933692557093760c7
jessicabarreirosm <jessica.martins@sptech.school>
2024-11-14 20:05:06 -0300

    first commit

---
+++ src/main/java/org/example/atividadepipeline/AtividadePipelineApplication.java
@@ -0,0 +1,13 @@
+package org.example.atividadepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadePipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/atividadepipeline/AtividadePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.atividadepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
